12931178d065dbf93241a474c870c17e4b9e6f50
Christian Bound <automaticbound@gmail.com>
2025-06-25 15:08:43 +0900

    icon, titling and mock data updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => (
   <header className="text-center mb-8">
-    <h1 className="text-4xl font-semibold mb-8 text-center text-indigo-700">ACTUAL 📄 LISTINGS</h1>
+    <h1 className="text-4xl font-semibold mb-8 text-center text-indigo-700">✍🏻MOCK📄O📄LISTINGS✍🏻</h1>
   </header>
 );
 
